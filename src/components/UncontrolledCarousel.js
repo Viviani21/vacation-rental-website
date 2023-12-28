80d5393c6acc292e453c4fbc10e06018e78a0d2e
Radi Galani <radi_d_1@yahoo.de>
2023-12-28 23:04:42 +0100

    little changes

--- src/components/UncontrolledCarousel.js
+++ src/components/UncontrolledCarousel.js
@@ -14,23 +14,23 @@ const UncontrolledCarousel = () => {
         <Carousel.Item>
           <ExampleCarouselImage imgUrl={FirstImg} text="First slide" />
           <Carousel.Caption>
-            <h3>First slide label</h3>
-            <p>Nulla vitae elit libero, a pharetra augue mollis interdum.</p>
+            <h3>Bulgaria</h3>
+            <p>Sofia</p>
           </Carousel.Caption>
         </Carousel.Item>
         <Carousel.Item>
           <ExampleCarouselImage imgUrl={SecondImg} text="Second slide" />
           <Carousel.Caption>
-            <h3>Second slide label</h3>
-            <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit.</p>
+            <h3>Greece</h3>
+            <p>Athen</p>
           </Carousel.Caption>
         </Carousel.Item>
         <Carousel.Item>
           <ExampleCarouselImage imgUrl={ThurdImg} text="Third slide" />
           <Carousel.Caption>
-            <h3>Third slide label</h3>
+            <h3>Italy</h3>
             <p>
-              Praesent commodo cursus magna, vel scelerisque nisl consectetur.
+              Rom
             </p>
           </Carousel.Caption>
         </Carousel.Item>
